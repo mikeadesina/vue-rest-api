b30664f6b3d3797f831b64231d1df5352c68affc
mikeadesina <michaeladeshina015@gmail.com>
2022-10-12 23:53:26 +0100

    laravel vue rest api

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/skills',
+      name: 'SkillIndex',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/skills/SkillIndex.vue')
+    },
+    {
+      path: '/skills/create',
+      name: 'SkillCreate',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/skills/SkillCreate.vue')
+    },
+    {
+      path: '/skills/:id/edit',
+      name: 'SkillEdit',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/skills/SkillEdit.vue'),
+      props:true
+    }
+
+  ]
+})
+
+export default router
---
+++ src/views/skills/SkillCreate.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { reactive } from "vue";
+import useSkills from "../../composables/skills";
+const { storeSkill, errors, skills } = useSkills();
+const form = reactive({
+  name: "",
+  slugs: "",
+})
+</script>
+
+<template>
+  <div class="mt-12">
+    <form class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md" @submit.prevent="storeSkill(form)">
+      <div class="space-y-6">
+        <div class="mb-6">
+          <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name
+          </label>
+          <input type="text" id="name" v-model="form.name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="john.doe@company.com">
+          <div v-if="errors.name">
+            <span class="text-sm text-red-400">
+              {{errors.name[0]}}
+            </span>
+          </div>
+        </div>
+        <div class="mb-6">
+          <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Slug</label>
+          <input type="text" id="slug" v-model="form.slug"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="•••••••••">
+          <div v-if="errors.slug">
+            <span class="text-sm text-red-400">
+              {{errors.slug[0]}}
+            </span>
+          </div>
+        </div>
+        <button type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      </div>
+
+    </form>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
